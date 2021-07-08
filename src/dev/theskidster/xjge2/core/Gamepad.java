10bdedc78915ab643402c7e19f41f3a1f0bc5c4c
theskidster <theskidster@gmail.com>
2021-07-07 22:06:08 -0500

    added Gamepad doc

--- src/dev/theskidster/xjge2/core/Gamepad.java
+++ src/dev/theskidster/xjge2/core/Gamepad.java
@@ -11,20 +11,40 @@ import org.lwjgl.system.MemoryStack;
  * Created: May 3, 2021
  */
 
+/**
+ * Represents a generic game controller held with two hands that exhibits two analog sticks, two or more triggers, and several buttons.
+ */
 final class Gamepad extends InputDevice {
 
     private GLFWGamepadState state;
     
+    /**
+     * Creates a new gamepad object and applies the users settings to its control configuration.
+     * 
+     * @param id          the unique number used to identify the device in other parts of the engine
+     * @param sensitivity a value used by gameplay systems to adjust the responsiveness of input actions based off the users preference
+     * @param deadzone    a value used to indicate how much pressure must be applied to an analog stick before its input is recognized
+     * @param config      a collection of various {@link Control} mappings
+     */
     Gamepad(int id, float sensitivity, float deadzone, HashMap<Control, Integer> config) {
         super(id, sensitivity, deadzone, config);
         validate();
     }
     
+    /**
+     * Creates a new gamepad object from an existing one.
+     * 
+     * @param gamepad the gamepad to copy 
+     */
     Gamepad(Gamepad gamepad) {
         super(gamepad);
         validate();
     }
 
+    /**
+     * Determines if this controller is eligible for use by the engine. GLFW recognizes Joysticks which exhibit predictable {@link Control} 
+     * layouts as Gamepads. Gamepads often include mappings for controls which correspond well to GLFWs definitions.
+     */
     private void validate() {
         if(glfwJoystickIsGamepad(id)) {
             name = glfwGetGamepadName(id);
