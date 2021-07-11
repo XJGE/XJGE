bf28a993cfe40067761c8688f25942efa631ec95
theskidster <theskidster@gmail.com>
2021-07-11 16:42:05 -0500

    added input device doc

--- src/dev/theskidster/xjge2/core/InputDevice.java
+++ src/dev/theskidster/xjge2/core/InputDevice.java
@@ -10,6 +10,9 @@ import java.util.Stack;
  * Created: May 3, 2021
  */
 
+/**
+ * Represents a peripheral device that can capture input actions from a user.
+ */
 abstract class InputDevice {
 
     final int id;
@@ -27,6 +30,14 @@ abstract class InputDevice {
     
     final HashMap<Control, Integer> config;
     
+    /**
+     * Creates a new input device object and applies the users settings to its control configuration.
+     * 
+     * @param id          the unique number used to identify the device in other parts of the engine
+     * @param sensitivity a value used by gameplay systems to adjust the responsiveness of input actions based off the users preference
+     * @param deadzone    a value used to indicate how much pressure must be applied to an analog stick before its input is recognized
+     * @param config      a collection of various {@link Control} mappings
+     */
     InputDevice(int id, float sensitivity, float deadzone, HashMap<Control, Integer> config) {
         this.id          = id;
         this.sensitivity = sensitivity;
@@ -34,6 +45,11 @@ abstract class InputDevice {
         this.config      = config;
     }
     
+    /**
+     * Creates a new input device object from an existing one.
+     * 
+     * @param inputDevice the input device to copy
+     */
     InputDevice(InputDevice inputDevice) {
         id              = inputDevice.id;
         sensitivity     = inputDevice.sensitivity;
@@ -45,8 +61,14 @@ abstract class InputDevice {
         config          = inputDevice.config;
     }
     
+    /**
+     * Processes the input actions of a device by looking for state changes in its interactive components. 
+     */
     protected abstract void poll();
     
+    /**
+     * Resolves a request to update the current {@link Puppet} object an input device is using.
+     */
     protected void resolvePuppetSetRequest() {
         if(puppetSetEvents.size() > 0) {
             puppets.push(puppetSetEvents.poll());
