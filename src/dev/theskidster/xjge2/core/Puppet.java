499322d7b376001f6e61dcb8e956fd6b73177f95
theskidster <theskidster@gmail.com>
2021-07-11 22:05:17 -0500

    completed Puppet doc

--- src/dev/theskidster/xjge2/core/Puppet.java
+++ src/dev/theskidster/xjge2/core/Puppet.java
@@ -7,12 +7,35 @@ import java.util.HashMap;
  * Created: May 3, 2021
  */
 
+/**
+ * Component object that enables implementing objects to make use of input actions captured from an input device by coupling 
+ * {@linkplain Control interactive components} to some {@linkplain Command meaningful action}.
+ * <br><br>
+ * Any object that can be controlled by a player should utilize a puppet object.
+ * 
+ * @see Input#setDevicePuppet(int, Puppet) 
+ * @see Input#bindPreviousPuppet(int)
+ */
 public final class Puppet {
 
     public final Object object;
     
+    /**
+     * A collection of command objects the puppet will use. Command definitions should exhibit the following structure:
+     * <blockquote><pre>
+     * puppet.commands.put(CROSS,    myCommand1());
+     * puppet.commands.put(TRIANGLE, myCommand2());
+     * puppet.commands.put(SQUARE,   myCommand3());
+     * </pre></blockquote>
+     */
     public final HashMap<Control, Command> commands = new HashMap<>();
     
+    /**
+     * Creates a new puppet object. It is expected that implementing objects will populate the puppets {@link commands} collection inside
+     * of its constructor following the puppet objects initialization.
+     * 
+     * @param object the implementing object to be controlled with this puppet
+     */
     public Puppet(Object object) {
         this.object = object;
     }
