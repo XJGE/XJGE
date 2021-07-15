29b7c3c52c4c3db94a3a02e7eb8a18de0930107b
theskidster <theskidster@gmail.com>
2021-07-14 22:39:19 -0500

    created structure for core package doc

--- src/dev/theskidster/xjge2/core/package-info.java
+++ src/dev/theskidster/xjge2/core/package-info.java
@@ -1,4 +1,72 @@
 /**
- * This package contains all the "core" classes of the engine
+ * Contains various classes, objects, and utiltites many of which are essential to function of the engine. These are listed in detail below:
+ * <br><br>
+ * <h2>Core Classes:</h2>
+ * <table border="1">
+ * <tr><td><b>Name</b></td><td><b>Description</b></td></tr>
+ * <tr><td>{@linkplain Hardware}</td><td>asdf</td></tr>
+ * <tr><td>{@linkplain Input}</td><td>asdf</td></tr>
+ * <tr><td>{@linkplain Monitor}</td><td>asdf</td></tr>
+ * <tr><td>{@linkplain Viewport}</td><td>asdf</td></tr>
+ * <tr><td>{@linkplain Window}</td><td>asdf</td></tr>
+ * </table>
+ * <br><br>
+ * <h2>Gameplay Utilities:</h2>
+ * <table border="1">
+ * <tr><td><b>Name</b></td><td><b>Description</b></td></tr>
+ * <tr><td>{@linkplain Audio}</td><td>asdf</td></tr>
+ * </table>
+ * <br><br>
+ * <h2>Other Miscellaneous Features:</h2>
+ * <table border="1">
+ * <tr><td><b>Name</b></td><td><b>Description</b></td></tr>
+ * <tr><td>{@linkplain ErrorUtils}</td><td>Provides convenience methods for locating errors encountered by the engine at runtime.</td></tr>
+ * </table>
  */
 package dev.theskidster.xjge2.core;
+
+
+/**
+ * Contains classes and objects essential to the function of the engine. These include:
+ * <ul>
+ * <li>{@link Hardware}</li>
+ * <li>{@link Input}</li>
+ * <li>{@link Monitor}</li>
+ * <li>{@link Viewport}</li>
+ * <li>{@link Window}</li>
+ * </ul>
+ * Additionally, this package contains various features useful for gameplay systems:
+ * <table><td><tr>
+ * <ul>
+ * <li>{@link Audio}</li>
+ * <li>{@link Camera}</li>
+ * <li>{@link Command}</li>
+ * <li>{@link Entity}</li>
+ * <li>{@link Event}</li>
+ * </ul>
+ * </td><td>
+ * <ul>
+ * <li>{@link Game}</li>
+ * <li>{@link InputDevice}</li>
+ * <li>{@link Observable}</li>
+ * <li>{@link Puppet}</li>
+ * <li>{@link Scene}</li>
+ * </ul>
+ * </td><td>
+ * <ul>
+ * <li>{@link Skybox}</li>
+ * <li>{@link Speaker}</li>
+ * <li>{@link Text}</li>
+ * <li>{@link Timer}</li>
+ * <li>{@link Widget}</li>
+ * </ul>
+ * </td></tr></table>
+ * Some other miscellaneous utilities are also worth mentioning here:
+ * <ul>
+ * <li>{@link ErrorUtils}</li>
+ * <li>{@link Font}</li>
+ * <li>{@link Skybox}</li>
+ * <li>{@link Song}</li>
+ * <li>{@link Sound}</li>
+ * </ul>
+ */
