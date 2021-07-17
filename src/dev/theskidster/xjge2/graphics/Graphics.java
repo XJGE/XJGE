1bb4db069038f0f704d39e1d86cb586a173afb33
theskidster <theskidster@gmail.com>
2021-07-16 20:21:04 -0500

    added Graphics doc

--- src/dev/theskidster/xjge2/graphics/Graphics.java
+++ src/dev/theskidster/xjge2/graphics/Graphics.java
@@ -11,6 +11,22 @@ import static org.lwjgl.opengl.GL30.*;
  * Created: May 7, 2021
  */
 
+/**
+ * Component object that supplies implementing objects with the following:
+ * <ul>
+ * <li>A vertex buffer object - As a default buffer through which the implementing class may supply its vertex data to the 
+ *                              {@linkplain dev.theskidster.xjge.shader.core graphics pipeline}.</li>
+ * <li>A index buffer object - To discourage data redundancy by specifying which vertices to reuse for the objects model mesh.</li>
+ * <li>A vertex array object - For providing convenient access to the default vertex buffer in addition to any the implementation may 
+ * define.</li>
+ * </ul>
+ * <p>
+ * Implementing objects should define their vertex data and vertex attribute layouts in their constructors using LWJGLs memory utilities 
+ * such as {@link org.lwjgl.system.MemoryUtil MemoryUtil} or {@link org.lwjgl.system.MemoryStack MemoryStack} (the later of which only 
+ * if the vertex data doesn't exceed the JVMs stack size). LWJGLs {@link org.lwjgl.assimp.Assimp Assimp} binding is also available in 
+ * conjunction to the previously mentioned classes to load vertex data in the form of a 3D model.
+ * </p>
+ */
 public final class Graphics {
 
     public final int vao = glGenVertexArrays();
@@ -22,6 +38,11 @@ public final class Graphics {
     
     public Matrix4f modelMatrix = new Matrix4f();
     
+    /**
+     * Convenience method provided to bind the default buffers defined by this class. Implementing classes are expected to define vertex 
+     * attribute layouts following this call in their constructors with methods like 
+     * {@link org.lwjgl.opengl.GL30#glVertexAttribPointer(int, int, int, boolean, int, java.nio.ByteBuffer) glVertexAttribPointer()}.
+     */
     public void bindBuffers() {
         glBindVertexArray(vao);
         
@@ -34,6 +55,24 @@ public final class Graphics {
         }
     }
     
+    /**
+     * Convenience method which frees the default buffer objects defined by this class. Additional buffers required by the implementing 
+     * object will need to be freed individually. This method should be called in conjunction with 
+     * {@link org.lwjgl.system.MemoryUtil#memFree(java.nio.Buffer) MemoryUtil.memFree()} if additional data was used during the 
+     * implementing objects lifetime.
+     */
+    public void freeBuffers() {
+        glDeleteVertexArrays(vao);
+        glDeleteBuffers(vbo);
+        glDeleteBuffers(ibo);
+    }
+    
+    /**
+     * Converts the matrix data structure provided by Assimp into a format the engine understands.
+     * 
+     * @param aiMatrix the Assimp matrix data we want to convert
+     * @return         a new four-component engine-friendly matrix object
+     */
     static Matrix4f convertFromAssimp(AIMatrix4x4 aiMatrix) {
         Matrix4f conversion = new Matrix4f();
         
@@ -58,12 +97,6 @@ public final class Graphics {
         conversion.m33(aiMatrix.d4());
         
         return conversion;
-    }
-    
-    public void freeBuffers() {
-        glDeleteVertexArrays(vao);
-        glDeleteBuffers(vbo);
-        glDeleteBuffers(ibo);
     }
     
 }
