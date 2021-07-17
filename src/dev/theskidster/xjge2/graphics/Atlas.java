e1b5856d794d407d6eb1266f364e5bff4cead411
theskidster <theskidster@gmail.com>
2021-07-16 19:46:46 -0500

    added atlas doc

--- src/dev/theskidster/xjge2/graphics/Atlas.java
+++ src/dev/theskidster/xjge2/graphics/Atlas.java
@@ -10,6 +10,18 @@ import org.joml.Vector2i;
  * Created: May 11, 2021
  */
 
+/**
+ * Represents a texture atlas (or sprite sheet) that will provide the texture coordinates of sub-images within a larger 
+ * {@link Texture}.
+ * <br><br>
+ * More specifically the data provided by this object includes:
+ * <ul>
+ * <li>The number of rows and columns the texture image was divided into.</li>
+ * <li>The number of sub-images (or cells) the atlas contains.</li>
+ * <li>The dimensions of a single sub-image cell as texture coordinates.</li>
+ * <li>A {@linkplain subImageOffsets collection} containing the texture coordinates of every sub-image cell.</li>
+ * </ul>
+ */
 public final class Atlas {
 
     public final int cellWidth;
@@ -18,13 +30,28 @@ public final class Atlas {
     public final int columns;
     public final int subImageCount;
     
-    public final float subImageWidth;
-    public final float subImageHeight;
+    /**
+     * The size (in texture coordinates) of a single sub-image cell. This information is useful in the event the atlas is used in a 
+     * {@link SpriteAnimation} where consecutive frames are placed next to one another.
+     */
+    public final float subImageWidth, subImageHeight;
     
     public final Vector2f texCoords = new Vector2f();
     
+    /**
+     * A collection that couples the cell locations of sub-images to their corresponding coordinates within the texture.
+     */
     public final Map<Vector2i, Vector2f> subImageOffsets;
     
+    /**
+     * Supplies the data generated from a texture and cell dimensions as a texture atlas. 
+     * <br><br>
+     * NOTE: The dimensions of the texture should be evenly divisible by the cell width and height.
+     * 
+     * @param texture    the texture image to use
+     * @param cellWidth  the width of each sub-image cell in pixels
+     * @param cellHeight the height of each sub-image cell in pixels
+     */
     public Atlas(Texture texture, int cellWidth, int cellHeight) {
         this.cellWidth  = cellWidth;
         this.cellHeight = cellHeight;
