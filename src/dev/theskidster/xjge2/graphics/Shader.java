44f225a9f533bd3953975b74769a648f3c766535
theskidster <theskidster@gmail.com>
2021-07-17 21:57:25 -0500

    added shader doc

--- src/dev/theskidster/xjge2/graphics/Shader.java
+++ src/dev/theskidster/xjge2/graphics/Shader.java
@@ -12,10 +12,26 @@ import static org.lwjgl.opengl.GL20.*;
  * Created: May 2, 2021
  */
 
+/**
+ * Encapsulates an OpenGL shader object. These objects contain the compiled results of a .glsl source file and provide it as a single stage 
+ * of a much larger {@link GLProgram}.
+ */
 public final class Shader {
     
     final int handle;
     
+    /**
+     * Parses a .glsl source file then utilizes the stage specified to produce a new OpenGL shader object.
+     * 
+     * @param filename the name of the .glsl file to parse. Expects the file extension to be included.
+     * @param stage    the stage of the shader process this object will describe. One of: 
+     *                 {@link org.lwjgl.opengl.GL30#GL_VERTEX_SHADER GL_VERTEX_SHADER}, 
+     *                 {@link org.lwjgl.opengl.GL30#GL_FRAGMENT_SHADER GL_FRAGMENT_SHADER}, 
+     *                 {@link org.lwjgl.opengl.GL32#GL_GEOMETRY_SHADER GL_GEOMETRY_SHADER}, 
+     *                 {@link org.lwjgl.opengl.GL40#GL_TESS_CONTROL_SHADER GL_TESS_CONTROL_SHADER}, 
+     *                 {@link org.lwjgl.opengl.GL40#GL_TESS_EVALUATION_SHADER GL_TESS_EVALUATION_SHADER}, or 
+     *                 {@link org.lwjgl.opengl.GL43#GL_COMPUTE_SHADER GL_COMPUTE_SHADER}. 
+     */
     public Shader(String filename, int stage) {
         StringBuilder output = new StringBuilder();
         InputStream file     = Shader.class.getResourceAsStream(XJGE.getAssetsFilepath() + filename);
