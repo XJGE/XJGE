4d37291de77f6bc20f1ed88251707e8cb16003db
theskidster <theskidster@gmail.com>
2021-07-17 22:07:40 -0500

    added SpriteAnimation doc

--- src/dev/theskidster/xjge2/graphics/SpriteAnimation.java
+++ src/dev/theskidster/xjge2/graphics/SpriteAnimation.java
@@ -10,6 +10,10 @@ import org.joml.Vector2i;
  * Created: Jun 13, 2021
  */
 
+/**
+ * Represents a 2D sprite animation that iterates over a series of sub-images from a {@link SpriteSheet} in sequence to create the illusion 
+ * of movement.
+ */
 public class SpriteAnimation {
 
     public int currFrame;
@@ -17,16 +21,32 @@ public class SpriteAnimation {
     
     public List<Vector2i> frames = new ArrayList<>();
     
+    /**
+     * Creates a new single-frame animation- a non-animation if you will.
+     * 
+     * @param frame the frame to display indefinitely until the animation is changed
+     */
     public SpriteAnimation(Vector2i frame) {
         speed = 1;
         frames.add(frame);
     }
     
+    /**
+     * Creates a new 2D animation using the frames provided.
+     * 
+     * @param frames the frames that comprise the animation in sequence
+     * @param speed  the speed of the animation in game ticks
+     */
     public SpriteAnimation(int speed, List<Vector2i> frames) {
         this.speed  = speed;
         this.frames = frames;
     }
     
+    /**
+     * Steps the animation forward changing the texture coordinates of the {@link SpriteSheet} and subsequently the sub-image rendered.
+     * 
+     * @param atlas the texture atlas (or sprite sheet) to use for this animation
+     */
     public void updateAnimation(Atlas atlas) {
         if(currFrame < frames.size()) {
             if(Game.tick(speed)) {
