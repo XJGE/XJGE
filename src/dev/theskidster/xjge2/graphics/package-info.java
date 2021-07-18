8c91e8b608861475e42dd61b7356cf2a63047062
theskidster <theskidster@gmail.com>
2021-07-17 22:18:35 -0500

    added graphics package info

--- src/dev/theskidster/xjge2/graphics/package-info.java
+++ src/dev/theskidster/xjge2/graphics/package-info.java
@@ -1,1 +1,16 @@
-
+/**
+ * Supplies abstractions of OpenGL into various objects and data structures to be used by other parts of the engine. This open-ended 
+ * decentralized approach to rendering enables implementing classes to make better use of the graphics pipeline while presenting more 
+ * opportunities for optimization.
+ * <p>Abstractions include: </p>
+ * <ul>
+ * <li>{@link Atlas}</li>
+ * <li>{@link Graphics}</li>
+ * <li>{@link Instance2D}</li>
+ * <li>{@link Model}</li>
+ * <li>{@link SkeletalAnimation}</li>
+ * <li>{@link SpriteAnimation}</li>
+ * <li>{@link Texture}</li>
+ * </ul>
+ */
+package dev.theskidster.xjge2.graphics;
