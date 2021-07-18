2f64b2712d8047af48af910d868c6c8bf5154976
theskidster <theskidster@gmail.com>
2021-07-17 21:34:35 -0500

    added node doc

--- src/dev/theskidster/xjge2/graphics/Node.java
+++ src/dev/theskidster/xjge2/graphics/Node.java
@@ -9,6 +9,11 @@ import org.joml.Matrix4f;
  * Created: Jun 17, 2021
  */
 
+/**
+ * Data structure that represents a single point of interest within a {@link Model} as defined by its file. Node objects are contained 
+ * within a hierarchy and provide a unique name that can be used to traverse the tree. Nodes may also exhibit parent/child relationships 
+ * with other nodes and are often used to calculate transformations in ascending order to move the {@link Bone bones} of an animated model.
+ */
 class Node {
 
     final String name;
@@ -17,11 +22,26 @@ class Node {
     List<Node> children       = new ArrayList<>();
     List<Matrix4f> transforms = new ArrayList<>();
     
+    /**
+     * Constructs a new node object that will couple the name of a node (most often corresponding to a {@link Bone}) to a parent if one 
+     * exists.
+     * 
+     * @param name   the unique name used identify the node in the hierarchy
+     * @param parent the parent node of this node
+     */
     Node(String name, Node parent) {
         this.name   = name;
         this.parent = parent;
     }
     
+    /**
+     * Calculates the transformation of a node relative to its parents.
+     * 
+     * @param node     the top level node to start calculating from
+     * @param frameNum the index of the animations current {@link KeyFrame}.
+     * 
+     * @return the final calculated transformation matrix
+     */
     static Matrix4f getParentTransform(Node node, int frameNum) {
         if(node == null) {
             return new Matrix4f();
@@ -39,6 +59,13 @@ class Node {
         }
     }
     
+    /**
+     * Locates a node within the hierarchy by using its unique name.
+     * 
+     * @param nodeName the name of the node to return
+     * 
+     * @return the node of this name or null if no such node exists
+     */
     Node getNodeByName(String nodeName) {
         Node result = null;
         
@@ -54,6 +81,12 @@ class Node {
         return result;
     }
     
+    /**
+     * Finds the total number of {@link KeyFrame KeyFrames} (represented as transformations) this node has in relation to the current 
+     * animation.
+     * 
+     * @return the number of transforms this node has in the current animation.
+     */
     int getNumKeyFrames() {
         int numFrames = transforms.size();
         
