890fb0eb1436b3964b0e92163a833360146fe19c
theskidster <theskidster@gmail.com>
2021-07-20 15:22:16 -0500

    revised package docs

--- src/dev/theskidster/xjge2/graphics/package-info.java
+++ src/dev/theskidster/xjge2/graphics/package-info.java
@@ -1,8 +1,11 @@
 /**
- * Supplies abstractions of OpenGL into various objects and data structures to be used by other parts of the engine. This open-ended 
- * decentralized approach to rendering enables implementing classes to make better use of the graphics pipeline while presenting more 
- * opportunities for optimization.
- * <p>Abstractions include: </p>
+ * Supplies abstractions of OpenGL into various objects and data structures to 
+ * be used by other parts of the engine. This open-ended decentralized approach 
+ * to rendering enables implementing classes to make better use of the graphics 
+ * pipeline while retaining more opportunities for optimization.
+ * <p>
+ * These Abstractions include:
+ * </p>
  * <ul>
  * <li>{@link Atlas}</li>
  * <li>{@link Graphics}</li>
