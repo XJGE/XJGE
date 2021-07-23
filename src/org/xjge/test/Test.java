9d5c8109ba4724af775cd0cdb37f0cab4d1445b1
theskidster <theskidster@gmail.com>
2021-07-22 19:47:52 -0500

    discovered bug that stretches the glViewport when Window.setDimensions() is called before XJGE.start()

--- src/org/xjge/test/Test.java
+++ src/org/xjge/test/Test.java
@@ -3,6 +3,7 @@ package org.xjge.test;
 import org.xjge.core.Game;
 import org.xjge.core.XJGE;
 import org.joml.Vector2i;
+import org.xjge.core.Window;
 
 /**
  * @author J Hoffman
@@ -24,7 +25,7 @@ public class Test {
         
         //Input.setDeviceEnabled(0, false);
         
-        //Window.setDimensions(1200, 400); TODO: stretched
+        Window.setDimensions(1200, 400);
         
         Game.setScene(new TestScene());
         
