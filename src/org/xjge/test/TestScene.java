cf7ccec275333750341f4be1a51f857708b03007
theskidster <theskidster@gmail.com>
2021-08-19 19:51:51 -0500

    tested feature added by previous commit

--- src/org/xjge/test/TestScene.java
+++ src/org/xjge/test/TestScene.java
@@ -1,11 +1,13 @@
 package org.xjge.test;
 
 import java.util.Map;
-import static org.lwjgl.glfw.GLFW.GLFW_CURSOR;
-import static org.lwjgl.glfw.GLFW.GLFW_CURSOR_HIDDEN;
 import org.xjge.core.Camera;
+import org.xjge.core.Command;
+import org.xjge.core.Control;
+import org.xjge.core.Input;
+import static org.xjge.core.Input.KEY_MOUSE_COMBO;
+import org.xjge.core.Puppet;
 import org.xjge.core.Scene;
-import org.xjge.core.Window;
 import org.xjge.graphics.GLProgram;
 
 /**
@@ -18,8 +20,32 @@ import org.xjge.graphics.GLProgram;
  */
 public class TestScene extends Scene {
 
+    class Move extends Command {
+
+        private String dir;
+        
+        Move(String dir) {
+           this.dir = dir; 
+        }
+        
+        @Override
+        public void execute() {
+            if(axisMoved()) {
+                System.out.println(dir + " " + getInputValue());
+            }
+        }
+        
+    }
+    
+    Puppet puppet = new Puppet(this);
+    
     public TestScene() {
         super("test");
+        
+        puppet.commands.put(Control.LEFT_STICK_X, new Move("x"));
+        puppet.commands.put(Control.LEFT_STICK_Y, new Move("y"));
+        
+        Input.setDevicePuppet(KEY_MOUSE_COMBO, puppet);
         
         //Window.setInputMode(GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
     }
