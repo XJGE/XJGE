711de428ee2a3f97c944286d776cb1090814ae2a
theskidster <theskidster@gmail.com>
2023-01-26 20:33:30 -0600

    this feature was better before

--- src/org/xjge/assets/palFragment.glsl
+++ src/org/xjge/assets/palFragment.glsl
@@ -5,5 +5,10 @@ in vec2 ioTexCoords;
 uniform sampler2D uTexture;
 
 void main() {
-    gl_FragColor = texture(uTexture, ioTexCoords);
+    
+	vec3 test = any(greaterThan(texture(uTexture, ioTexCoords).rgb, vec3(100))) ? vec3(1) : vec3(0);
+	
+	vec3 outputColor = vec3(0.5f); //= any(greaterThan(texture(uTexture, ioTexCoords).rgb, vec3(100))) ? vec3(1) : vec3(0);
+    
+    gl_FragColor = vec4(outputColor, 1);
 }
--- src/org/xjge/main/Main.java
+++ src/org/xjge/main/Main.java
@@ -2,9 +2,16 @@ package org.xjge.main;
 
 //Created: Jan 25, 2023
 
+import java.util.LinkedList;
 import org.joml.Vector2i;
+import static org.lwjgl.glfw.GLFW.GLFW_JOYSTICK_1;
+import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
+import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
 import org.xjge.core.Game;
 import org.xjge.core.XJGE;
+import org.xjge.graphics.BufferType;
+import org.xjge.graphics.GLProgram;
+import org.xjge.graphics.Shader;
 
 
 /**
@@ -18,7 +25,20 @@ public class Main {
         XJGE.init("/org/xjge/assets/", "org.xjge.test.", new Vector2i(16, 9));
         Game.setScene(new TestScene());
         
-        
+        {
+            var shaderSourceFiles = new LinkedList<Shader>() {{
+                add(new Shader("palVertex.glsl", GL_VERTEX_SHADER));
+                add(new Shader("palFragment.glsl", GL_FRAGMENT_SHADER));
+            }};
+            
+            PaletteProcess paletteShader = new PaletteProcess(new GLProgram(shaderSourceFiles, "palette"));
+                
+            paletteShader.glProgram.use();
+            paletteShader.glProgram.addUniform(BufferType.INT,  "uTexture");
+            paletteShader.glProgram.addUniform(BufferType.MAT4, "uProjection");
+            
+            XJGE.usePostProcessShader(GLFW_JOYSTICK_1, paletteShader);
+        }
         
         XJGE.start();
         
