115e8826f17c870e2a090f6389c1d6b476c6c210
theskidster <theskidster@gmail.com>
2023-01-29 20:21:37 -0600

    added main class- testing palette again

---
+++ src/org/xjge/main/Main.java
@@ -0,0 +1,15 @@
+package org.xjge.main;
+
+//Created: Jan 28, 2023
+
+/**
+ * @author J Hoffman
+ * @since  
+ */
+public class Main {
+
+    /*
+    Entity textures are kept grayscale then color is applied using lookup table and seperate texture.    
+    */
+    
+}
