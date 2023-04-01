fe59227f51509414f3fcfbf1f734809c8bd0dedf
theskidster <theskidster@gmail.com>
2023-04-01 11:16:45 -0500

    Altered default color palette to better correspond to OS/expected industry standards

--- src/org/xjge/graphics/Color.java
+++ src/org/xjge/graphics/Color.java
@@ -14,21 +14,21 @@ import org.xjge.core.XJGE;
 public final class Color {
     
     public static final Color WHITE   = new Color(1f);
-    public static final Color SILVER  = new Color(191);
-    public static final Color GRAY    = new Color(127);
+    public static final Color SILVER  = new Color(192);
+    public static final Color GRAY    = new Color(128);
     public static final Color BLACK   = new Color(0f);
     public static final Color RED     = new Color(255,   0,   0);
-    public static final Color BROWN   = new Color(127,  63,   0);
+    public static final Color BROWN   = new Color(128,  64,   0);
     public static final Color YELLOW  = new Color(255, 255,   0);
-    public static final Color ORANGE  = new Color(255, 127,   0);
+    public static final Color ORANGE  = new Color(255, 128,   0);
     public static final Color GREEN   = new Color(  0, 255,   0);
-    public static final Color EMERALD = new Color(  0, 127,   0);
+    public static final Color EMERALD = new Color(  0, 128,   0);
     public static final Color CYAN    = new Color(  0, 255, 255);
-    public static final Color TEAL    = new Color(  0, 127, 127);
+    public static final Color TEAL    = new Color(  0, 128, 128);
     public static final Color BLUE    = new Color(  0,   0, 255);
-    public static final Color NAVY    = new Color(  0,   0, 127);
+    public static final Color NAVY    = new Color(  0,   0, 128);
     public static final Color MAGENTA = new Color(255,   0, 255);
-    public static final Color PURPLE  = new Color(127,   0, 127);
+    public static final Color PURPLE  = new Color(128,   0, 128);
     
     public final float r;
     public final float g;
