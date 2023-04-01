f27e7754a4c47fec2c793e18ef6c786205e76cdd
theskidster <theskidster@gmail.com>
2023-04-01 11:50:14 -0500

    Updated engine version to 2.2.0- generated new jar files

---
+++ dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.xjge.core"},{"l":"org.xjge.graphics"}];updateSearchResults();
--- src/org/xjge/core/XJGE.java
+++ src/org/xjge/core/XJGE.java
@@ -92,7 +92,7 @@ public final class XJGE {
     private static boolean firstMouse = true;
     
     public static final Path PWD       = Path.of("").toAbsolutePath();
-    public static final String VERSION = "2.1.18";
+    public static final String VERSION = "2.2.0";
     
     private static Split split = Split.NONE;
     
