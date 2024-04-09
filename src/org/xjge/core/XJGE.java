93b38e3c9d65a1c5187de95e9c8b0fee31b0e40b
theskidster <theskidster@gmail.com>
2024-04-09 07:38:39 -0500

    Increased engine version to include Font hotfix from last commit

--- src/org/xjge/core/XJGE.java
+++ src/org/xjge/core/XJGE.java
@@ -92,7 +92,7 @@ public final class XJGE {
     private static boolean firstMouse = true;
     
     public static final Path PWD       = Path.of("").toAbsolutePath();
-    public static final String VERSION = "2.4.4";
+    public static final String VERSION = "2.4.5";
     
     private static Split split = Split.NONE;
     
