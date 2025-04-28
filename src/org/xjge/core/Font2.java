0b73d8fa435a2e91f267d496673d54c12a359805
J Hoffman <theskidster@gmail.com>
2025-04-27 20:31:59 -0500

    Rearranged array data from Font2.loadVectorData() to correspond to the order of its final fields

--- src/org/xjge/core/Font2.java
+++ src/org/xjge/core/Font2.java
@@ -19,39 +19,51 @@ import static org.lwjgl.system.MemoryUtil.NULL;
  */
 public final class Font2 {
 
+    final boolean isBitmapFont;
+    
     private final float SCALE = 1.5f;
     
     private static final int DEFAULT_SIZE = 32;
     
-    private final int textureHandle;
-    
-    public final int size;
-    public final int largestGlyphWidth;
+    final int size;
+    final int textureHandle;
+    final int largestGlyphWidth;
+    final int bitmapWidth;
+    final int bitmapHeight;
     
     private static final Font2 placeholder = new Font2("/org/xjge/assets/", "font_source_code_pro.ttf", DEFAULT_SIZE);
     
     private Font2(String filepath, String filename, int size) {
         int[] info = loadVectorFont(filepath, filename, size);
         
-        this.size           = info[0];
-        textureHandle       = info[1];
-        largestGlyphWidth   = info[2];
+        isBitmapFont        = info[0] == 1;
+        this.size           = info[1];
+        textureHandle       = info[2];
+        largestGlyphWidth   = info[3];
+        bitmapWidth         = info[4];
+        bitmapHeight        = info[5];
     }
     
     public Font2(String filename, int size) {
         this(XJGE.getAssetsFilepath(), filename, size);
     }
     
+    private int[] loadBitmapFont(String filepath, String filename, int size) {
+        //TODO: reimplement .bmf file parsing
+        return null;
+    }
+    
     private int[] loadVectorFont(String filepath, String filename, int size) {
         try(InputStream file = Font.class.getResourceAsStream(filepath + filename)) {
-            int[] info = new int[3];
+            int[] info = new int[6];
             
             if(size <= 0 || size > 128) {
                 throw new IllegalStateException("Invalid font size used. Font size must be between 1 and 128");
             }
             
-            info[0] = size;
-            info[1] = glGenTextures();
+            info[0] = 0;
+            info[1] = size;
+            info[2] = glGenTextures();
             
             try(MemoryStack stack = MemoryStack.stackPush()) {
                 byte[] data = file.readAllBytes();
@@ -63,7 +75,7 @@ public final class Font2 {
                     throw new IllegalStateException("Failed to parse font information from file");
                 }
                 
-                float pixelScale   = stbtt_ScaleForPixelHeight(fontInfo, info[0]);
+                float pixelScale   = stbtt_ScaleForPixelHeight(fontInfo, info[1]);
                 int[] advanceWidth = new int[1];
                 int[] leftBearing  = new int[1];
                 
@@ -78,26 +90,24 @@ public final class Font2 {
                 for(int i = 0; i < charset.length(); i++) {
                     stbtt_GetCodepointHMetrics(fontInfo, charset.charAt(i), advanceWidth, leftBearing);
                     float scaledAdvance = advanceWidth[0] * pixelScale;
-                    if(scaledAdvance > info[2]) info[2] = Math.round(scaledAdvance * SCALE);
+                    if(scaledAdvance > info[3]) info[3] = Math.round(scaledAdvance * SCALE);
                 }
                 
                 boolean containsAllGlyphs = false;
                 int bitmapSizeInPixels    = 128;
-                int bitmapWidth           = 0;
-                int bitmapHeight          = 0;
                 
                 ByteBuffer imageBuffer = null;
                 STBTTPackedchar.Buffer packedCharBuffer = STBTTPackedchar.malloc(charset.length());
                 
                 while(!containsAllGlyphs) {
-                    bitmapWidth  = Math.round(bitmapSizeInPixels * SCALE);
-                    bitmapHeight = Math.round(bitmapSizeInPixels * SCALE);
-                    imageBuffer  = MemoryUtil.memAlloc(bitmapWidth * bitmapHeight);
+                    info[4]     = Math.round(bitmapSizeInPixels * SCALE);
+                    info[5]     = Math.round(bitmapSizeInPixels * SCALE);
+                    imageBuffer = MemoryUtil.memAlloc(info[4] * info[5]);
                     
                     try(STBTTPackContext context = STBTTPackContext.malloc()) {
-                        stbtt_PackBegin(context, imageBuffer, bitmapWidth, bitmapHeight, 0, (int) (info[2] * SCALE), NULL);
+                        stbtt_PackBegin(context, imageBuffer, info[4], info[5], 0, (int) (info[3] * SCALE), NULL);
                         stbtt_PackSetOversampling(context, 1, 1);
-                        containsAllGlyphs = stbtt_PackFontRange(context, fontBuffer, 0, info[0], 32, packedCharBuffer);
+                        containsAllGlyphs = stbtt_PackFontRange(context, fontBuffer, 0, info[1], 32, packedCharBuffer);
                         stbtt_PackEnd(context);
                     }
                     
@@ -107,8 +117,8 @@ public final class Font2 {
                     bitmapSizeInPixels += 16;
                 }
                 
-                glBindTexture(GL_TEXTURE_2D, info[1]);
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, bitmapWidth, bitmapHeight, 0, GL_ALPHA, GL_UNSIGNED_BYTE, imageBuffer);
+                glBindTexture(GL_TEXTURE_2D, info[2]);
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, info[4], info[5], 0, GL_ALPHA, GL_UNSIGNED_BYTE, imageBuffer);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
                 glBindTexture(GL_TEXTURE_2D, 0);
@@ -123,9 +133,12 @@ public final class Font2 {
             Logger.logWarning("Failed to load font \"" + filename + "\" a placeholder will be used instead", exception);
             
             return new int[] {
+                0,
                 DEFAULT_SIZE,
                 placeholder.textureHandle,
-                placeholder.largestGlyphWidth
+                placeholder.largestGlyphWidth,
+                placeholder.bitmapWidth,
+                placeholder.bitmapHeight
             };
         }
     }
