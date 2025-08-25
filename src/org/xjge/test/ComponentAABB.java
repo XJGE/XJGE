14528a38fb482628596bdb83fcb3c802eb28b036
J Hoffman <theskidster@gmail.com>
2025-08-25 14:00:29 -0500

    Added new component for AABB collision- we'll implement this later

---
+++ src/org/xjge/test/ComponentAABB.java
@@ -0,0 +1,20 @@
+package org.xjge.test;
+
+import org.xjge.core.Component;
+
+/**
+ * 
+ * @author J Hoffman
+ * @since 
+ */
+class ComponentAABB extends Component {
+
+    int width;
+    int height;
+    
+    ComponentAABB(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+}
