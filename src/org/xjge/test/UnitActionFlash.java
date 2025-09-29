ab4e1272388aae5dafb3054682d5d8ede7c6420d
J Hoffman <theskidster@gmail.com>
2025-09-28 21:12:48 -0500

    Pushing changes from last week that I forgot about- been busy

---
+++ src/org/xjge/test/UnitActionFlash.java
@@ -0,0 +1,17 @@
+package org.xjge.test;
+
+/**
+ * 
+ * @author J Hoffman
+ * @since 
+ */
+class UnitActionFlash extends UnitAction {
+
+    @Override
+    boolean perform(TurnContext turnContext) {
+        
+        
+        return false;
+    }
+
+}
