9cd7cb075325cba24b397e8a61132249175262b9
J Hoffman <theskidster@gmail.com>
2025-10-13 16:57:09 -0500

    Added control binding for keyboard to switch LEFT_SHIFT to Q to cancel actions, implemented damage and corresponding widget to manabolt spell

--- src/org/xjge/test/Main.java
+++ src/org/xjge/test/Main.java
@@ -1,8 +1,33 @@
 package org.xjge.test;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
 import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
+import org.xjge.core.Control;
+import static org.xjge.core.Control.CIRCLE;
+import static org.xjge.core.Control.CROSS;
+import static org.xjge.core.Control.DPAD_DOWN;
+import static org.xjge.core.Control.DPAD_LEFT;
+import static org.xjge.core.Control.DPAD_RIGHT;
+import static org.xjge.core.Control.DPAD_UP;
+import static org.xjge.core.Control.L1;
+import static org.xjge.core.Control.L2;
+import static org.xjge.core.Control.L3;
+import static org.xjge.core.Control.LEFT_STICK_X;
+import static org.xjge.core.Control.LEFT_STICK_Y;
+import static org.xjge.core.Control.OPTIONS;
+import static org.xjge.core.Control.PS_BUTTON;
+import static org.xjge.core.Control.R1;
+import static org.xjge.core.Control.R2;
+import static org.xjge.core.Control.R3;
+import static org.xjge.core.Control.RIGHT_STICK_X;
+import static org.xjge.core.Control.RIGHT_STICK_Y;
+import static org.xjge.core.Control.SHARE;
+import static org.xjge.core.Control.SQUARE;
+import static org.xjge.core.Control.TRIANGLE;
+import org.xjge.core.Input;
 import org.xjge.core.Window;
 import org.xjge.core.XJGE;
 import org.xjge.graphics.Color;
@@ -92,6 +117,10 @@ public class Main {
         XJGE.addCommand("setGameMode", setGameMode);
         
         XJGE.addObserver(setGameMode);
+        
+        var controlBindings = new HashMap<Control, Integer>(){{}};
+        controlBindings.put(CIRCLE, GLFW_KEY_Q);
+        Input.setDeviceControlBindings(Input.KEYBOARD, controlBindings);
         
         XJGE.setClearColor(Color.SILVER);
         XJGE.setScene(new Scene3D("map_test.txt"));
--- src/org/xjge/test/UnitActionManabolt.java
+++ src/org/xjge/test/UnitActionManabolt.java
@@ -1,6 +1,8 @@
 package org.xjge.test;
 
+import java.util.Random;
 import static org.lwjgl.glfw.GLFW.GLFW_JOYSTICK_1;
+import org.xjge.core.Timer;
 import org.xjge.core.UI;
 
 /**
@@ -11,9 +13,13 @@ import org.xjge.core.UI;
 class UnitActionManabolt extends UnitAction {
 
     private boolean success;
+    private boolean delayOver;
     
     private int stage;
     
+    private final Timer fireDelay = new Timer();
+    private final Timer finishDelay = new Timer();
+    private final Random random = new Random();
     private final ComponentUnit targetUnit;
     private WidgetManabolt widget;
     
@@ -28,6 +34,7 @@ class UnitActionManabolt extends UnitAction {
                 if(widget == null) {
                     widget = new WidgetManabolt(turnContext);
                     UI.addWidget(GLFW_JOYSTICK_1, "manabolt_minigame", widget);
+                    stage = 1;
                 }
             }
             case 1 -> {
@@ -37,10 +44,31 @@ class UnitActionManabolt extends UnitAction {
                 }
             }
             case 2 -> {
-                if(success) {
-                    //hit!
+                if(!delayOver) {
+                    //TODO: add particle effects
+                    
+                    if(fireDelay.tick(5, 12, true) && fireDelay.getTime() == 4) {
+                        delayOver = true;
+                    }
                 } else {
-                    //miss
+                    WidgetDamage damageWidget;
+                
+                    if(success) {
+                        damageWidget = new WidgetDamage(random.nextInt(2), "-3hp");
+                        targetUnit.health -= 3;
+                    } else {
+                        damageWidget = new WidgetDamage(random.nextInt(2), "miss!");
+                    }
+                    
+                    UI.addWidget(GLFW_JOYSTICK_1, "damage_" + damageWidget.uuid, damageWidget);
+
+                    stage = 3;
+                }
+            }
+            case 3 -> {
+                if(finishDelay.tick(7, 12, true) && finishDelay.getTime() == 6) {
+                    turnContext.scene.setCameraFollow(turnContext.unit, 0.5f);
+                    return true;
                 }
             }
         }
