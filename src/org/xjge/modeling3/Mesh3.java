5ce66a7b4b3829c3516c57c163a457b0a4a1afce
J Hoffman <theskidster@gmail.com>
2026-03-16 14:57:19 -0500

    Moved extraction methods out of Model3 and into Mesh3 class

--- src/org/xjge/modeling3/Mesh3.java
+++ src/org/xjge/modeling3/Mesh3.java
@@ -1,5 +1,12 @@
 package org.xjge.modeling3;
 
+import java.nio.IntBuffer;
+import org.lwjgl.assimp.AIBone;
+import org.lwjgl.assimp.AIFace;
+import org.lwjgl.assimp.AIMesh;
+import org.lwjgl.assimp.AIVector3D;
+import static org.xjge.modeling3.Model3.MAX_BONES_PER_VERTEX;
+
 /**
  * 
  * @author J Hoffman
@@ -20,5 +27,117 @@ public final class Mesh3 {
     
     public int[] boneIDs;
     public int[] indices;
+    
+    private enum VertexAttribute {
+        
+        POSITION(3),
+        NORMAL(3),
+        TANGENT(3),
+        UV(2);
+        
+        final int stride;
+        
+        VertexAttribute(int stride) { this.stride = stride; }
+        
+    }
+    
+    Mesh3(AIMesh aiMesh, Skeleton3 skeleton) {
+        positions = extractVertexAttribute(aiMesh, VertexAttribute.POSITION);
+        normals   = extractVertexAttribute(aiMesh, VertexAttribute.NORMAL);
+        tangents  = extractVertexAttribute(aiMesh, VertexAttribute.TANGENT);
+        uvs       = extractVertexAttribute(aiMesh, VertexAttribute.UV);
+        extractBoneWeights(aiMesh, skeleton);
+        materialIndex = aiMesh.mMaterialIndex();
+        extractIndices(aiMesh);
+    }
+    
+    private float[] extractVertexAttribute(AIMesh aiMesh, VertexAttribute attribute) {
+        AIVector3D.Buffer buffer;
+        
+        switch(attribute) {
+            case POSITION -> buffer = aiMesh.mVertices();
+            case NORMAL   -> buffer = aiMesh.mNormals();
+            case TANGENT  -> buffer = aiMesh.mTangents();
+            case UV       -> buffer = aiMesh.mTextureCoords(0);
+            default       -> throw new IllegalArgumentException("Unsupported vertex attribute found: \"" + attribute + "\"");
+        }
+        
+        if(buffer == null) return null;
+        
+        int vertexCount = aiMesh.mNumVertices();
+        float[] result  = new float[vertexCount * attribute.stride];
+        
+        for(int i = 0; i < vertexCount; i++) {
+            var vec = buffer.get(i);
+            
+            result[i * attribute.stride]     = vec.x();
+            result[i * attribute.stride + 1] = vec.y();
+            
+            if(attribute.stride == 3) result[i * attribute.stride + 2] = vec.z();
+        }
+        
+        return result;
+    }
+    
+    private void extractIndices(AIMesh aiMesh) {
+        int faceCount = aiMesh.mNumFaces();
+        var faces     = aiMesh.mFaces();
+        
+        indices = new int[faceCount * 3];
+        
+        for(int i = 0; i < faceCount; i++) {
+            AIFace aiFace      = faces.get(i);
+            IntBuffer idx      = aiFace.mIndices();
+            indices[i * 3]     = idx.get(0);
+            indices[i * 3 + 1] = idx.get(1);
+            indices[i * 3 + 2] = idx.get(2);
+        }
+    }
+    
+    private void extractBoneWeights(AIMesh aiMesh, Skeleton3 skeleton) {
+        int vertexCount = aiMesh.mNumVertices();
+        int boneCount   = aiMesh.mNumBones();
+        var aiBones     = aiMesh.mBones();
+        
+        boneIDs     = new int[vertexCount * MAX_BONES_PER_VERTEX];
+        boneWeights = new float[vertexCount * MAX_BONES_PER_VERTEX];
+        
+        for(int boneIndex = 0; boneIndex < boneCount; boneIndex++) {
+            var aiBone   = AIBone.create(aiBones.get(boneIndex));
+            var weights  = aiBone.mWeights();
+            var boneName = aiBone.mName().dataString();
+            
+            //Blender likes to add pipes for some reason so we'll discard that if we see it
+            int pipe = boneName.indexOf("|");
+            if(pipe != -1) boneName = boneName.substring(pipe + 1);
+            
+            var mapIndex = skeleton.boneMap.get(boneName);
+            
+            //Build resolver list, this helps us convert local mesh space to global space during rendering
+            if(mapIndex == null) {
+                mapIndex          = skeleton.bones.size();
+                var bone          = new Bone3();
+                bone.name         = boneName;
+                bone.offsetMatrix = Model3.convertMatrix(aiBone.mOffsetMatrix());
+                skeleton.bones.add(bone);
+                skeleton.boneMap.put(boneName, mapIndex);
+            }
+            
+            for(int w = 0; w < aiBone.mNumWeights(); w++) {
+                var vw       = weights.get(w);
+                int vertexID = vw.mVertexId();
+                float weight = vw.mWeight();
+                int base     = vertexID * MAX_BONES_PER_VERTEX;
+                
+                for(int i = 0; i < MAX_BONES_PER_VERTEX; i++) {
+                    if(boneWeights[base + i] == 0f) {
+                        boneIDs[base + i]     = mapIndex;
+                        boneWeights[base + i] = weight;
+                        break;
+                    }
+                }
+            }
+        }
+    }
     
 }
--- src/org/xjge/modeling3/Model3.java
+++ src/org/xjge/modeling3/Model3.java
@@ -2,21 +2,17 @@ package org.xjge.modeling3;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import org.joml.Matrix4f;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.assimp.AIAnimation;
-import org.lwjgl.assimp.AIBone;
-import org.lwjgl.assimp.AIFace;
 import org.lwjgl.assimp.AIMaterial;
 import org.lwjgl.assimp.AIMatrix4x4;
 import org.lwjgl.assimp.AIMesh;
 import org.lwjgl.assimp.AINode;
 import org.lwjgl.assimp.AINodeAnim;
 import org.lwjgl.assimp.AIScene;
-import org.lwjgl.assimp.AIVector3D;
 import org.lwjgl.assimp.Assimp;
 import org.lwjgl.system.MemoryUtil;
 import org.xjge.core.Asset;
@@ -40,19 +36,6 @@ public final class Model3 extends Asset {
     private List<Material3> materials;
     private List<Mesh3> meshes;
     private List<SkeletalAnimation3> animations;
-    
-    private enum VertexAttribute {
-        
-        POSITION(3),
-        NORMAL(3),
-        TANGENT(3),
-        UV(2);
-        
-        final int stride;
-        
-        VertexAttribute(int stride) { this.stride = stride; }
-        
-    }
     
     public static final Model3 load(String filename) {
         var defaultFlags = Assimp.aiProcess_Triangulate | Assimp.aiProcess_GenSmoothNormals |
@@ -129,104 +112,6 @@ public final class Model3 extends Asset {
         }
     }
     
-    private void extractBoneWeights(AIMesh aiMesh, Mesh3 mesh) {
-        int vertexCount  = aiMesh.mNumVertices();
-        mesh.boneIDs     = new int[vertexCount * MAX_BONES_PER_VERTEX];
-        mesh.boneWeights = new float[vertexCount * MAX_BONES_PER_VERTEX];
-        var aiBones      = aiMesh.mBones();
-        int boneCount    = aiMesh.mNumBones();
-        
-        for(int boneIndex = 0; boneIndex < boneCount; boneIndex++) {
-            var aiBone   = AIBone.create(aiBones.get(boneIndex));
-            var weights  = aiBone.mWeights();
-            var boneName = aiBone.mName().dataString();
-            
-            //Blender likes to add pipes for some reason so we'll discard that if we see it
-            int pipe = boneName.indexOf("|");
-            if(pipe != -1) boneName = boneName.substring(pipe + 1);
-            
-            var mapIndex = skeleton.boneMap.get(boneName);
-            
-            //Build resolver list, this helps us convert local mesh space to global space during rendering
-            if(mapIndex == null) {
-                mapIndex          = skeleton.bones.size();
-                var bone          = new Bone3();
-                bone.name         = boneName;
-                bone.offsetMatrix = convertMatrix(aiBone.mOffsetMatrix());
-                skeleton.bones.add(bone);
-                skeleton.boneMap.put(boneName, mapIndex);
-            }
-            
-            for(int w = 0; w < aiBone.mNumWeights(); w++) {
-                var vw       = weights.get(w);
-                int vertexID = vw.mVertexId();
-                float weight = vw.mWeight();
-                int base     = vertexID * MAX_BONES_PER_VERTEX;
-                
-                for(int i = 0; i < MAX_BONES_PER_VERTEX; i++) {
-                    if(mesh.boneWeights[base + i] == 0f) {
-                        mesh.boneIDs[base + i]     = mapIndex;
-                        mesh.boneWeights[base + i] = weight;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    
-    private float[] extractVertexAttribute(AIMesh aiMesh, VertexAttribute attribute) {
-        AIVector3D.Buffer buffer;
-        
-        switch(attribute) {
-            case POSITION -> buffer = aiMesh.mVertices();
-            case NORMAL   -> buffer = aiMesh.mNormals();
-            case TANGENT  -> buffer = aiMesh.mTangents();
-            case UV       -> buffer = aiMesh.mTextureCoords(0);
-            default       -> throw new IllegalArgumentException("Unsupported vertex attribute found: \"" + attribute + "\"");
-        }
-        
-        if(buffer == null) return null;
-        
-        int vertexCount = aiMesh.mNumVertices();
-        float[] result  = new float[vertexCount * attribute.stride];
-        
-        for(int i = 0; i < vertexCount; i++) {
-            var vec = buffer.get(i);
-            
-            result[i * attribute.stride]     = vec.x();
-            result[i * attribute.stride + 1] = vec.y();
-            
-            if(attribute.stride == 3) result[i * attribute.stride + 2] = vec.z();
-        }
-        
-        return result;
-    }
-    
-    private int[] extractIndices(AIMesh aiMesh) {
-        int faceCount = aiMesh.mNumFaces();
-        int[] indices = new int[faceCount * 3];
-        var faces     = aiMesh.mFaces();
-        
-        for(int i = 0; i < faceCount; i++) {
-            AIFace aiFace      = faces.get(i);
-            IntBuffer idx      = aiFace.mIndices();
-            indices[i * 3]     = idx.get(0);
-            indices[i * 3 + 1] = idx.get(1);
-            indices[i * 3 + 2] = idx.get(2);
-        }
-        
-        return indices;
-    }
-    
-    private Matrix4f convertMatrix(AIMatrix4x4 aiMatrix) {
-        return new Matrix4f().set(
-            aiMatrix.a1(), aiMatrix.b1(), aiMatrix.c1(), aiMatrix.d1(),
-            aiMatrix.a2(), aiMatrix.b2(), aiMatrix.c2(), aiMatrix.d2(),
-            aiMatrix.a3(), aiMatrix.b3(), aiMatrix.c3(), aiMatrix.d3(),
-            aiMatrix.a4(), aiMatrix.b4(), aiMatrix.c4(), aiMatrix.d4()
-        );
-    }
-    
     private List<Material3> parseMaterials(AIScene aiScene) {
         List<Material3> result    = new ArrayList<>();
         PointerBuffer aiMaterials = aiScene.mMaterials();
@@ -249,17 +134,7 @@ public final class Model3 extends Asset {
         
         for(int i = 0; i < aiScene.mNumMeshes(); i++) {
             var aiMesh = AIMesh.create(aiMeshes.get(i));
-            var mesh   = new Mesh3();
-            
-            mesh.positions = extractVertexAttribute(aiMesh, VertexAttribute.POSITION);
-            mesh.normals   = extractVertexAttribute(aiMesh, VertexAttribute.NORMAL);
-            mesh.tangents  = extractVertexAttribute(aiMesh, VertexAttribute.TANGENT);
-            mesh.uvs       = extractVertexAttribute(aiMesh, VertexAttribute.UV);
-            extractBoneWeights(aiMesh, mesh);
-            mesh.materialIndex = aiMesh.mMaterialIndex();
-            mesh.indices       = extractIndices(aiMesh);
-            
-            result.add(mesh);
+            result.add(new Mesh3(aiMesh, skeleton));
         }
         
         return result;
@@ -299,6 +174,15 @@ public final class Model3 extends Asset {
         }
         
         return result;
+    }
+    
+    static Matrix4f convertMatrix(AIMatrix4x4 aiMatrix) {
+        return new Matrix4f().set(
+            aiMatrix.a1(), aiMatrix.b1(), aiMatrix.c1(), aiMatrix.d1(),
+            aiMatrix.a2(), aiMatrix.b2(), aiMatrix.c2(), aiMatrix.d2(),
+            aiMatrix.a3(), aiMatrix.b3(), aiMatrix.c3(), aiMatrix.d3(),
+            aiMatrix.a4(), aiMatrix.b4(), aiMatrix.c4(), aiMatrix.d4()
+        );
     }
     
     public final Skeleton3 getSkeleton() {
