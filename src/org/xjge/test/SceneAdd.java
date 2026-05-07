336d3cc66bfd8b11b03613d9f143e8f743a0bb32
J Hoffman <theskidster@gmail.com>
2026-05-07 11:19:44 -0500

    Changed updated jar names, recopied test package

---
+++ src/org/xjge/test/SceneAdd.java
@@ -0,0 +1,78 @@
+package org.xjge.test;
+
+import java.util.Map;
+import static org.lwjgl.glfw.GLFW.GLFW_JOYSTICK_2;
+import org.xjge.core.Camera;
+import org.xjge.core.Entity;
+import org.xjge.core.Scene;
+import org.xjge.core.Skybox;
+import org.xjge.core.Window;
+import org.xjge.graphics.Shader;
+import org.xjge.graphics.Texture;
+
+/**
+ * 
+ * @author J Hoffman
+ * @since 
+ */
+public class SceneAdd extends Scene {
+
+    private boolean subscribed;
+    private float angle;
+    private Skybox skybox;
+    
+    public SceneAdd() {
+        super("test");
+        
+        var entityA = new Entity();
+        var entityB = new Entity().addComponent(new CompTestA()).addComponent(new CompTestB());
+        var entityC = new Entity().addComponent(new CompTestA()).addComponent(new Prism(0, 0, 10, 1, 1, 1));
+        
+        System.out.println("EntityA: " + entityA.uuid);
+        System.out.println("EntityB: " + entityB.uuid);
+        System.out.println("EntityC: " + entityC.uuid);
+        
+        addEntity(entityA);
+        addEntity(entityB);
+        addEntity(entityC);
+        
+        var right  = Texture.load("sky_right.png");
+        var left   = Texture.load("sky_left.png");
+        var top    = Texture.load("sky_top.png");
+        var bottom = Texture.load("sky_bottom.png");
+        var front  = Texture.load("sky_front.png");
+        var back   = Texture.load("sky_back.png");
+        var back2  = Texture.load("sky_back.png");
+        
+        Window.setViewportCamera(GLFW_JOYSTICK_2, new CameraOrtho());
+        
+        skybox = new Skybox(right, left, top, bottom, front, back, false);
+        setSkybox(skybox);
+        //XJGE.enableBloom = true;
+        //XJGE.setClearColor(Color.BLACK);
+        //skybox.setBloomThreshold(0.9f);
+    }
+
+    @Override
+    public void update(double targetDelta, double trueDelta) {
+        for(var entity : queryEntities(CompTestA.class)) {
+            
+        }
+    }
+
+    @Override
+    public void render(int viewportID, Camera camera, int depthTexHandle) {
+        for(var entity : queryEntities(Prism.class)) {
+            entity.getComponent(Prism.class).render(camera);
+        }
+    }
+
+    @Override
+    public void renderShadows(Shader depthProgram) {
+    }
+
+    @Override
+    public void exit() {
+    }
+
+}
---
+++ src/org/xjge/test/UISlider.java
@@ -0,0 +1,75 @@
+package org.xjge.test;
+
+import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_LEFT;
+import org.xjge.core.Mouse;
+import org.xjge.graphics.Color;
+import org.xjge.graphics.ModelAnimator;
+import org.xjge.ui.Rectangle;
+
+/**
+ * 
+ * @author J Hoffman
+ * @since 
+ */
+final class UISlider {
+    
+    private boolean dragging = false;
+    
+    public Rectangle track = new Rectangle(0, 0, 300, 6);
+    public Rectangle thumb = new Rectangle(0, 0, 10, 24);
+    
+    private final ModelAnimator animator;
+    
+    private final Color thumbColor = new Color(0.75f);
+    
+    UISlider(ModelAnimator animator) {
+        this.animator = animator;
+    }
+    
+    void update() {
+        if(!dragging) {
+            double time = animator.getNormalizedTime();
+            thumb.positionX = (int) (track.positionX + time * track.width) - 5;
+        }
+    }
+    
+    void render() {
+        track.render(1f, Color.GRAY);
+        thumb.render(1f, thumbColor);
+    }
+    
+    void relocate(Rectangle parent) {
+        track.positionX = parent.positionX + 10;
+        track.positionY = parent.positionY + parent.height - 120;
+        thumb.positionY = track.positionY - 9;
+    }
+    
+    void processMouseInput(Mouse mouse) {
+        thumbColor.set(mouse.hovered(thumb) ? 1f : 0.75f);
+        
+        float mx = (float) mouse.getCursorPositionX();
+        float my = (float) mouse.getCursorPositionY();
+        
+        boolean hovering = mx >= track.positionX &&
+                           mx <= track.positionX + track.width &&
+                           my >= track.positionY - 10 &&
+                           my <= track.positionY + track.height + 10;
+        
+        if(mouse.clicked() && mouse.getButtonID() == GLFW_MOUSE_BUTTON_LEFT) {
+            if(hovering || dragging) {
+                dragging = true;
+                
+                float time = (mx - track.positionX) / track.width;
+                time = Math.max(0f, Math.min(1f, time));
+                
+                animator.setNormalizedTime(time);
+                thumb.positionX = (int) (track.positionX + time * track.width) - 5;
+                
+                animator.pause();
+            }
+        } else {
+            dragging = false;
+        }
+    }
+    
+}
